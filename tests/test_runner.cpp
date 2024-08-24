96a3c80fbca67a8d6eec5954a3bbf1053980a852
Daniel Kettle <initial.dann@gmail.com>
2024-08-24 16:01:41 -0600

    Removed tests/ cmake file, compile executable for testing in top level

---
+++ tests/test_runner.cpp
@@ -0,0 +1,27 @@
+#include <QtTest/QtTest>
+#include <QtCore/QDebug>
+#include <QtCore/QObject>
+
+int main(int argc, char *argv[])
+{
+    Q_UNUSED(argc);
+    Q_UNUSED(argv);
+
+    int result = 0;
+//    QVariantSlideTest *slideTests = new QVariantSlideTest();
+//    result = QTest::qExec(slideTests);
+
+//    QVariantPresentationTest *presTests = new QVariantPresentationTest();
+//    result = QTest::qExec(presTests);
+
+//    SlideFileSpecV1 *slideSpecV1Tests = new SlideFileSpecV1();
+//    result = QTest::qExec(slideSpecV1Tests);
+
+//    PresentationFileSpecV1 *presSpecV1Tests = new PresentationFileSpecV1();
+//    result = QTest::qExec(presSpecV1Tests);
+
+//    TestMenuGeneration *menuTests = new TestMenuGeneration();
+//    result = QTest::qExec(menuTests);
+
+    return result;
+}
---
+++ tests/version.h
@@ -0,0 +1,24 @@
+#ifndef SUNDAYBEACON_TESTS_VERSION_H
+#define SUNDAYBEACON_TESTS_VERSION_H
+
+// Compiler accessible definitions
+
+#define SUNDAYBEACON_TESTS_VERSION_RELEASE "0"
+#define SUNDAYBEACON_TESTS_VERSION_MAJOR "0"
+#define SUNDAYBEACON_TESTS_VERSION_MINOR "1"
+#define SUNDAYBEACON_TESTS_VERSION_PATCH "0"
+
+// Program accessible definitions
+
+namespace SundayBeacon {
+    namespace Testing {
+	namespace Version {
+            const int RELEASE = 0;
+            const int MAJOR = 0;
+            const int MINOR = 1;
+            const int PATCH = 0;
+	}
+    }
+}
+
+#endif // SUNDAYBEACON_TESTS_VERSION_H
